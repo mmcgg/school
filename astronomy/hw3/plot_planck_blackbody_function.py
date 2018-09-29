9972cb62dfdfa9f3c747d3fe679aab518bf0ad1f
Phil Hyatt <phyatt16@gmail.com>
2018-09-28 18:22:19 -0600

    Changes to be committed: 	new file:   plot_planck_blackbody_function.py

---
+++ astronomy/hw3/plot_planck_blackbody_function.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+T = 5700
+v = np.logspace(0,16,10000)
+B = np.zeros(len(v))
+c = 3e8
+k = 1.38e-23
+h = 6.626e-34
+
+for i in xrange(0,len(v)-1):
+    
+    B[i] = 2*h*pow(v[i],3.0)/(pow(c,2)*np.exp((h*v[i])/(k*T))-1)
+
+plt.loglog(v,B,basex=10,basey=10)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Brightness (W/m^2/Hz/sr)')
+plt.title('Blackbody radiation curve for a star at 5700K')
+plt.show()
