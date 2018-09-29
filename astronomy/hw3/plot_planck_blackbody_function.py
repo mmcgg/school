caf250a99a5d9b6ffa4258dcf23402d9b20142d8
Phil Hyatt <phyatt16@gmail.com>
2018-09-29 12:44:06 -0600

    Changes to be committed: 	modified:   plot_planck_blackbody_function.py

--- astronomy/hw3/plot_planck_blackbody_function.py
+++ astronomy/hw3/plot_planck_blackbody_function.py
@@ -17,4 +17,36 @@ plt.loglog(v,B,basex=10,basey=10)
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Brightness (W/m^2/Hz/sr)')
 plt.title('Blackbody radiation curve for a star at 5700K')
+plt.ion()
 plt.show()
+
+# ------------ Exponential Decay Plotting ----------------- #
+T = 1.248e9
+t = 4.5*np.linspace(0,1e9,10000)
+N_K0 = 1
+
+N_K = N_K0*pow(2,-t/T)
+N_Ar = 1-N_K0*pow(2,-t/T)
+ratio = N_Ar/N_K
+
+plt.figure()
+plt.plot(t,N_K)
+plt.plot(t,N_Ar)
+plt.legend(['K','Ar'])
+plt.xlabel('Time (years)')
+plt.ylabel('Abundance')
+plt.title('Exponential decay of Potassium 40')
+plt.show()
+
+plt.figure()
+plt.plot(t,ratio)
+plt.legend(['Ar/K'])
+plt.xlabel('Time (years)')
+plt.ylabel('Ratio')
+plt.title('Parent/Daughter Ratio')
+# plt.axis([0,4.5e9,0,100])
+plt.axhline(y=3)
+plt.show()
+plt.pause(10000.0)
+
+
